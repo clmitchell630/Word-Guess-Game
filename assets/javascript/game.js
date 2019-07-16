364eb5c7b4e41c68a767149171d8510af5e0d45e
cmitchell <clmitchell630@gmail.com>
2019-07-15 19:51:22 -0500

    user input check to random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\nHello!");
+console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\n  Hello!");
 
 var champList = [
     "Alistar",
@@ -41,36 +41,57 @@ var champList = [
 var wins = 0;
 var loss = 0;
 var guesses = 10;
-
+var randomChamp;
 document.addEventListener('DOMContentLoaded', function () {
 
-    //initialization AKA game start
+    //initialization AKA game start and everything else....
     document.getElementById("inProgress").style.display = "none";
     document.getElementById("gameWin").style.display = "none";
     document.getElementById("gameLoss").style.display = "none";
 
     window.addEventListener("keyup", function (event) {
-        if (event.key === "Enter") {
+        var userInput = event.key.toLowerCase();
+
+        if (userInput === "enter") {
             event.preventDefault();
             document.getElementById("gameStart").style.display = "none";
             document.getElementById("inProgress").style.display = "block";
             pickChamp();
         }
-        
+       
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            //event.keyCode is a letter
+            this.console.log("event keycode is " + event.keyCode);
+            var index = randomChamp.toLowerCase().indexOf(userInput);
+            if (index > -1) {
+                //input exists in randomchamp
+                console.log("letter at " + index + " is " + randomChamp[index]);
+            }
+            else {
+
+            }
+
+        }
+
+        this.console.log(event.keyCode + " " + userInput);
+
     });
 
     function pickChamp() {
-        var champSelect = champList[Math.floor(Math.random() * champList.length)];
-        this.console.log(champSelect);
+        randomChamp = champList[Math.floor(Math.random() * champList.length)];
+        this.console.log(randomChamp);
 
         var blankString = "";
-        for (var i = 0; i < champSelect.length; i++) {
+        for (var i = 0; i < randomChamp.length; i++) {
             blankString = blankString + "_ ";
         }
 
         document.getElementById("mysteryPick").innerHTML = blankString;
     }
-    
+
+
+
+
 
 
 
